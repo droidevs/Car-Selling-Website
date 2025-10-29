eae0367ba4ff04f9daf3444cb156d98111903278
Mouad Oumous <mouadoumous@gmail.com>
2025-10-29 11:19:43 +0100

    boom

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,11 @@
-<div class="card">
-    <div class="card-header">{{ $title }}</div>
-    {{ $slot }}
+
+@dump($attributes)
+<div {{ $attributes->merge(['lang' => 'ka'])->class("card card-text-$color card-bg-$bgColor") }}>
+    <div {{ $title->attributes->class('card-header') }} >{{ $title }}</div>
+    @if ($slot->isEmpty())
+        <p>Please provide some content</p>
+    @else
+        {{  $slot }}
+    @endif
     <div class="card-footer">{{ $footer }}</div>
 </div>
