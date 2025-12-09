91426778fb52eb93933b5c398dfa22b1dff488fc
Mouad Oumous <mouadoumous@gmail.com>
2025-12-09 19:51:35 +0100

    added users seeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com'
+        ]);
+
+        User::factory()
+        ->count(3)
+        ->create();
+    }
+}
