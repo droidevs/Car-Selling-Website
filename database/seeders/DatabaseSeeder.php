8e8b9d664e4fde0e5227e5b19feaa003c306af42
Mouad Oumous <mouadoumous@gmail.com>
2025-12-09 20:13:30 +0100

    call all seeders inside app seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UsersSeeder::class,
+            StatesSeeder::class,
+            MakersSeeder::class,
+            FuelTypesSeeder::class,
+            CarTypesSeeder::class,
+            CarsSeeder::class,
+            FavouriteCarsSeeder::class
         ]);
     }
 }
